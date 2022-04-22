3dfa038b5744605de0a65b5d175c915f0d25c406
Ken Mitchell <contact@ken-mitchell.com>
2022-04-22 13:05:39 -0400

    Made domlog_ips work with just one user too

--- bin/domlog_ips.sh
+++ bin/domlog_ips.sh
@@ -1,19 +1,33 @@
-# Show the quantity of logs for each domlog file
+# Show the quantity of logs for each ip address in the 
+# domlog file unique by date and IP.
 #
-# user@host #> sudo domlog_hits.sh
+# user@host #> sudo domlog_ips.sh
 #
-# 1129 example1.com
-# 1123 example2.com
-# 123 example3.com
+# 1129 12/APR/2020 example1.com
+# 1066 13/APR/2020 example1.com
+# 198 12/APR/2020 example2.com
 
-for i in `find /etc/apache2/logs/domlogs -maxdepth 1 -type f|egrep -v 'offset|_log$|proxy'`
-do
-        awk '{print $1, $4}' $i|
+do_filter () {
+        awk '{print $1, $4}' $1|
         cut -d: -f1|
 	awk '{print $2, $1}'|
 	cut -d[ -f2 |
 	sort|
         uniq -c|
         sort -n|
-        awk '{print $1, $2, $3 " '`echo $i|cut -d/ -f6`'"}'
-done|sort -nr
+        awk '{print $1, $2, $3 " '`echo $1|cut -d/ -f6`'"}'
+}
+
+file=${1// }
+if [[ -z $file ]]
+then
+	for i in `find /etc/apache2/logs/domlogs -maxdepth 1 -type f|egrep -v 'offset|_log$|proxy'`
+	do
+		do_filter $i
+	done|sort -nr
+else
+	do_filter $file|sort -nr
+
+fi
+
+
