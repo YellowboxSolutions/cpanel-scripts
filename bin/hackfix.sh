51b1441543219b681b5aaac8d933bec6ec1e7428
Ken Mitchell <contact@ken-mitchell.com>
2022-04-23 07:58:13 -0400

    hackfix still WIP

--- bin/hackfix.sh
+++ bin/hackfix.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 user=${1// }
-if [[ -z $user ]]; then
-  echo "wrong params"
-  echo "hacksearch.sh user"
-  exit 0
+if [[ -z $user ]]
+then
+	user=$(stat . -c %U)
+	trust=false
+else
+	trust=true
 fi
-
 
 echo "*********************************"
 echo "* hackfix $user"
@@ -15,16 +16,16 @@ echo "*********************************"
 userdir=/home/$user
 
 echo "Remove directories in ~/etc"
-sudo -u $user -- find $userdir/etc -type d \( -iname *anonymousFox* -o -iname *smtpfox* -o -iname *pwcache* \) -exec rm -rf {} \;
+find $userdir/etc -type d \( -iname *anonymousFox* -o -iname *smtpfox* -o -iname *pwcache* \) -exec rm -rf {} \;
 
 echo "Remove files in ~/mail"
-sudo -u $user -- find $userdir/mail \( -iname *anonymousFox* -o -iname *smtpfox* -o -iname *pwcache* \) -exec rm -rf {} \;
+find $userdir/mail \( -iname *anonymousFox* -o -iname *smtpfox* -o -iname *pwcache* \) -exec rm -rf {} \;
 
 echo "Remove bad entries from ~/etc/shadow"
 if [ -f "$userdir/etc/shadow" ]; then
-	sudo -u $user -- mv -f $userdir/etc/shadow $userdir/etc/shadow.old
-	#sudo -u $user -- grep -v $userdir/etc/shadow.old > $userdir/etc/shadow
-	#sudo -u $user -- rm $userdir/etc/shadow.old
+	mv -f $userdir/etc/shadow $userdir/etc/shadow.old
+	grep -v $userdir/etc/shadow.old > $userdir/etc/shadow
+	rm $userdir/etc/shadow.old
 fi
 
 
