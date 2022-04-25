6b0ee9ab9f79f697118343b0b88bbca8d0296ed1
Ken Mitchell <contact@ken-mitchell.com>
2022-04-25 14:46:04 -0400

    Separate checks and allow disabling or running one at a time. Runs as the user.

--- bin/hacksearch.sh
+++ bin/hacksearch.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 do_checks () {
-
-
-
 	echo "Checking cPanel contact email"
 	cat /home/$duser/.contactemail
 	echo "."
@@ -58,35 +55,42 @@ do_checks () {
 } 2>&1
 
 do_wpval () {
-
 	echo "Checking wordpress user"
-	sudo -u $duser -- /usr/local/bin/php /usr/bin/wp --path=/home/$duser/public_html user list
+	/usr/local/bin/php /usr/bin/wp --path=/home/$duser/public_html user list
 	echo "."
 	echo ""
 
 	echo "Checking wordpress checksums"
 	echo "* core"
-	sudo -u $duser -- /usr/local/bin/php /usr/bin/wp --path=/home/$duser/public_html core verify-checksums 
+	/usr/local/bin/php /usr/bin/wp --path=/home/$duser/public_html core verify-checksums 
 	echo "* plugins"
-	sudo -u $duser -- /usr/local/bin/php /usr/bin/wp --path=/home/$duser/public_html plugin verify-checksums --all 
+	/usr/local/bin/php /usr/bin/wp --path=/home/$duser/public_html plugin verify-checksums --all 
 	echo "."
 	echo ""
 } 2>&1
 
 do_files () {
 	echo "Getting access-logs"
-	mkdir -p $PWD/access-logs >/dev/null 2>&1
-	sudo cp /home/$duser/logs/*.gz $PWD/access-logs 
-	sudo cp /home/$duser/access-logs/* $PWD/access-logs
-	sudo chmod -R +r $PWD
+	mkdir -p $PWD/access-log-copies >/dev/null 2>&1
+	cp /home/$duser/logs/*.gz $PWD/access-log-copies/
+	cp /home/$duser/access-logs/* $PWD/access-log-copies/
 	echo "."
 	echo ""
 } 2>&1
 
 die() { echo "$*" >&2; exit 2; }  # complain to STDERR and exit with error
 needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
+no_tests() { checks=false;files=false;wpval=false; }
+all_tests() { checks=true;files=true;wpval=true; }
 
-while getopts c:u:-: OPT; do
+# Fail if running as root
+if [ "$(id -u)" -eq 0 ]; then
+	die "Cannot be ran as root"
+fi
+all_tests
+
+
+while getopts cfwm:u:-: OPT; do
 	# support long options: https://stackoverflow.com/a/28466267/519360
 	if [ "$OPT" = "-" ]; then   # long option: reformulate OPT and OPTARG
 		OPT="${OPTARG%%=*}"       # extract long option name
@@ -94,22 +98,37 @@ while getopts c:u:-: OPT; do
 		OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
 	fi
 	case "$OPT" in
-		a | alpha )    alpha=true ;;
-		m | min )      mmin="$OPTARG" ;; # number of minutes to look for modified files
-		u | user )    needs_arg; duser="$OPTARG" ;; # number of minutes to look for modified files
-		??* )          die "Illegal option --$OPT" ;;  # bad long option
-		? )            exit 2 ;;  # bad short option (error reported via getopts)
+		c | checks )    no_tests;checks=true ;;
+		f | files )     no_tests;files=true ;;
+		nochecks )      checks=false ;;
+		nofiles )       files=false ;;
+		nowpval )       wpval=false ;;
+		w | wpval )     no_tests;wpval=true ;;
+		m | mmin )      mmin="$OPTARG" ;; # number of minutes to look for modified files
+		u | user )      needs_arg; duser="$OPTARG" ;; # number of minutes to look for modified files
+		??* )           die "Illegal option --$OPT" ;;  # bad long option
+		? )             exit 2 ;;  # bad short option (error reported via getopts)
 	esac
 done
 shift $((OPTIND-1)) # remove parsed options and args from $@ list
 
 if [[ -z $duser ]]; then
-	die "--user is required"
+	duser=$USER
+	if [ "$DUSER" = "root" ]; then
+		die "Cannot be ran as root"
+	fi
+fi
+
+if [[ -n $mmin ]]; then
+	if ! checks; then
+		die "mmin can only be used when 'checks' are enabled"
+	fi
 fi
 
 if [[ -z $mmin ]]; then
 	mmin="-60"
 fi
+
 
 echo "*********************************"
 echo "* Checking $duser"
@@ -117,13 +136,26 @@ echo "*********************************"
 
 echo "  * Doing checks"
 echo "*********************************"
-do_checks
+if $checks; then
+	do_checks
+else
+	echo "skipping"
+fi
 
 echo "  * Doing wp validations"
 echo "*********************************"
-do_wpval
+if $wpval; then
+
+	do_wpval
+else
+	echo "skipping"
+fi
 
 echo "  * Doing log copy"
 echo "*********************************"
-do_files
+if $files; then
+	do_files
+else
+	echo "skipping"
+fi
 
